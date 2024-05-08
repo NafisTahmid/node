416ce8b8a42b19b836e6f32c2b643610770c86eb
Lenovo <nafistahmid4@gmail.com>
2024-05-08 23:14:26 +0600

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Assalamu-Alaikum Mom! Learning Node Js!')
+const vegetables = [
+  {
+    id:1,
+    name:"Cabbage",
+    color:"Green",
+    price:30
+  },
+  {
+    id:2,
+    name:"Carrot",
+    color:"Red",
+    price:50
+  },
+  {
+    id:3,
+    name:"Tomato",
+    color:"Red",
+    price:45
+  },
+  {
+    id:4,
+    name:"Potato",
+    color:"brown",
+    price:40
+  },
+  {
+    id:5,
+    name:"Onion",
+    color:"Purple",
+    price:"90"
+  },
+  {
+    id:6,
+    name:"Cucumber",
+    color:"Green",
+    price:"40"
+  }
+];
+
+app.get('/vegetables', (req, res) => {
+  res.send(vegetables)
 })
+
+app.get('/', (req, res) => res.send("Welcome to our vegetable shop!"))
+
+app.get('/vegetables/:id', (req, res) => {
+  const id = req.params.id
+  const vegetable = vegetables[id]
+  res.send(vegetable)
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
